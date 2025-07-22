b27dd08a64d2e0bac3f573e4935446cb03b77950
Kanishk Sachdev <kanishksachdev@gmail.com>
2025-07-22 07:13:48 +0530

    hardcoding public env var

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,6 +1,6 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+posthog.init("phc_d5zDFPQokbIKMjFyawWWEvGUzTK4PzOn6ae1xxXuaX3", {
   api_host: "/ingest",
   ui_host: "https://us.posthog.com",
   defaults: "2025-05-24",
