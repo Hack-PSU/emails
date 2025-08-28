614a6b8ff4511fd0d57bd31f1051702c6f923ff4
Kanishk Sachdev <kanishksachdev@gmail.com>
2025-08-28 14:28:01 -0400

    fix dev auth

--- src/common/context/LayoutProvider.tsx
+++ src/common/context/LayoutProvider.tsx
@@ -21,7 +21,7 @@ export default function LayoutProvider({
     <>
       <FirebaseProvider>
         <QueryClientProvider client={queryClient}>
-          <AuthGuard config={{minimumRole: Role.TEAM}}>{children}</AuthGuard>
+          <AuthGuard config={{ minimumRole: Role.TEAM }}>{children}</AuthGuard>
         </QueryClientProvider>
       </FirebaseProvider>
     </>
